30fe35a3e225d1750a9155f05cd24b5cd27f0cec
Zack Amis <zachariah.amis@vizientinc.com>
2022-08-19 10:20:59 -0500

    first commit

---
+++ day2/day2.js
@@ -0,0 +1,81 @@
+// console.log("string")
+// console.log(1234)
+// console.log(true)
+// console.log(null)
+// console.log({})
+// console.log(['Trevor','Kelly', 'Shawn', '58','Bonnie',
+// 'Karel','chris', 'kyle', 'Gabriel', 'Heath',])
+
+// let zack = {
+//     name: 'zack',
+//     age: 33,
+//     skills: ['javascript', 'fathering', 'casting firebolt', 'being somewhat of a nerd']
+// }
+// console.log(zack.skills[0])
+
+
+
+// const constVariable = 'const'
+// var varVariable = 'var'
+
+// constVariable != 'new stuff'
+
+// console.log(constVariable)
+// console.log(varVariable)
+
+// variables are pointers or signs to things, and not the things themselves.
+// // they are designed to be a placeholder to interpret logic until the real value comes across
+
+
+
+// document.querySelector('h1').innerText = letVariable
+// let sizeOfNumber= ''
+// if(Math.random() > 0.5) {
+//    sizeOfNumber = 'BIG number'
+// } else {
+//     sizeOfNumber = 'little number'
+// }
+// document.querySelector('h1').innerText = sizeOfNumber
+// const data = ['some', 'random', 'text']
+// let data2 = ['more', 'cool', 'text']
+// const nestedARray = [data, data2]
+
+
+// nestedARray.forEach(array => {
+//     for (const item of array) {
+//         console.log(item)
+        
+//     }
+// })
+
+
+
+
+// for (const word in data) {
+//     console.log(word + ' is the place in index!')
+//     const newP = document.createElement('p')
+//     newP.innerText = word 
+//     document.querySelector('body').appendChild(newP)
+// }
+
+// data.forEach(word => {
+//     console.log(word + ' from the loop')
+//     const newP = document.createElement('p')
+//     newP.innerText = word 
+//     document.querySelector('body').appendChild(newP)
+// })
+
+
+// for (let index = 0; index < data.length; index++) {
+//     const element = data[index];
+//     console.log(element)
+// }
+
+
+// ''
+// ""
+// `this is the data: ${data}`
+
+// 'this is the data: ' + data
+
+// if()
