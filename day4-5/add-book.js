b052e32cfccb5c5324a0e781b07f86f116a45f3c
Zack Amis <zachariah.amis@vizientinc.com>
2022-09-08 19:37:46 -0500

    update for local storage

---
+++ day4-5/add-book.js
@@ -0,0 +1,11 @@
+document.querySelector('#home-button').addEventListener('click', () => navigate('./index.html'))
+document.querySelector('#submit-button').addEventListener('click', () => {
+    let title = document.querySelector('#add-book-title')
+    let author = document.querySelector("#add-book-author")
+    let image = document.querySelector("#add-book-image")
+    addNewBook({
+        title: title.value,
+        author: author.value,
+        imageURL: image.value
+    }) 
+})
--- day4-5/helper.js
+++ day4-5/helper.js
@@ -1,3 +1,30 @@
 function navigate(url) {
     location.href = url
 }
+
+function addNewBook(book) {
+    const books = tryToGetBooks()
+    console.log(books)
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+}
+
+function tryToGetBooks() {
+    try {
+        return JSON.parse(localStorage.getItem('books')) || []
+    } catch(e) {
+        console.error('oopsie', e)
+        return []
+    }
+}
+
+
+
+
+
+
+
+
+
+
